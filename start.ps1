486fa5d0f1bc5f9deb31650b2143ef4487cfec2c
vinaum8 <vinaumpt@gmail.com>
2023-05-04 15:28:00 -0300

    Deploy kind cluster with nginx ingress on Windows

---
+++ start.ps1
@@ -0,0 +1,31 @@
+# Permissão de executar scripts no windows
+Set-ExecutionPolicy Unrestricted
+
+# Install chocolatey on Windows
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Instalar pré requisitos para criação de um cluster local
+choco install kind --force -y
+choco install kubernetes-cli -y
+choco install docker -y
+choco install docker-desktop -y
+choco install git -y
+choco install helm -y
+
+kind create cluster --config .\kind\kind-start-config.yaml
+
+kubectl cluster-info --context kind-k8s-learning
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait --namespace ingress-nginx `
+  --for=condition=ready pod `
+  --selector=app.kubernetes.io/component=controller `
+  --timeout=90s
+
+kubectl apply -f .\deploy-app\deployment.yaml
+
+Write-Output "Acesse os links no navegador para validar o funcionamento do cluster com Nginx - Ingress"
+Write-Output "http://localhost/bar"
+Write-Output "http://localhost/foo"
+
